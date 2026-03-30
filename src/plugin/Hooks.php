77816d8da8e169416ed337c63cd0a79fdbbffedd
bitski <18152029+bitski@users.noreply.github.com>
2026-03-30 13:42:12 +0200

    refactor(theme): rename setupClasses + setupOption to configClasses + configOption after Setup.php rename

--- src/plugin/Hooks.php
+++ src/plugin/Hooks.php
@@ -33,7 +33,7 @@ class Hooks
      */
     protected function registerCssClassesHooks(): void
     {
-        foreach (Config::$classes as $filter => $setupClasses) {
+        foreach (Config::$classes as $filter => $configClasses) {
             add_filter($filter, function ($defaultClasses = '', $merge = true) use ($filter) {
                 // Returns classes as a space-separated string.
                 return $this->getClassesByFilter($filter, $defaultClasses, $merge);
@@ -44,7 +44,7 @@ class Hooks
     /**
      * Getter for CSS classes by filter name.
      * Returns a space-separated string of classes.
-     * Merges setup classes with default classes if $merge is true.
+     * Merges config classes with default classes if $merge is true.
      * Otherwise, returns default classes only.
      *
      * @param  string  $filter
@@ -55,10 +55,10 @@ class Hooks
      */
     public function getClassesByFilter(string $filter, array $defaultClasses = [], bool $merge = true): string
     {
-        // Get setup classes if they're set and not empty.
-        $setupClasses = [];
+        // Get config classes if they're set and not empty.
+        $configClasses = [];
         if (isset(Config::$classes[$filter]) && ! empty(Config::$classes[$filter])) {
-            $setupClasses = Config::$classes[$filter];
+            $configClasses = Config::$classes[$filter];
         }
 
         // Ensure $defaultClasses is an array.
@@ -66,10 +66,10 @@ class Hooks
             $defaultClasses = [];
         }
 
-        // If the $merge parameter is set to true, merge setup classes with default classes.
+        // If the $merge parameter is set to true, merge config classes with default classes.
         // Returns merged classes as a space-separated string.
         if ($merge) {
-            $merged_classes = array_filter(array_unique(array_merge($setupClasses, $defaultClasses)));
+            $merged_classes = array_filter(array_unique(array_merge($configClasses, $defaultClasses)));
 
             return implode(' ', $merged_classes);
         }
@@ -83,7 +83,7 @@ class Hooks
      */
     protected function registerOptionHooks(): void
     {
-        foreach (Config::$options as $filter => $setupOption) {
+        foreach (Config::$options as $filter => $configOption) {
             add_filter($filter, function ($defaultOption = null) use ($filter) {
                 // Returns the option value or the default option if set.
                 return $this->getOptionByFilter($filter, $defaultOption);
@@ -97,7 +97,7 @@ class Hooks
      * Otherwise, checks global Config options.
      *
      * @param  string  $filter
-     * @param  mixed  $defaultOption  (default: null, for a fallback to global setup option)
+     * @param  mixed  $defaultOption  (default: null, for a fallback to global config option)
      *
      * @return mixed
      */
@@ -115,15 +115,15 @@ class Hooks
             }
         }
 
-        // Returns a setup option if it's set and not empty.
+        // Returns a config option if it's set and not empty.
         if (isset(Config::$options[$filter]) && Config::$options[$filter] !== '') {
-            $setupOption = Config::$options[$filter];
+            $configOption = Config::$options[$filter];
 
-            if (is_array($setupOption) || is_bool($setupOption)) {
-                return $setupOption;
+            if (is_array($configOption) || is_bool($configOption)) {
+                return $configOption;
             }
 
-            return (string)$setupOption;
+            return (string)$configOption;
         }
 
         // Returns a default option as a fallback.
