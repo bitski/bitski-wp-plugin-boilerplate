9ce039be5f599f0de0c19b67269486289083b57e
bitski <18152029+bitski@users.noreply.github.com>
2026-05-20 15:12:31 +0200

    feat(core): add `Helpers` class with example string utility method

---
+++ src/core/Helpers.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin helpers.
+ *
+ * @since 0.7.0
+ */
+
+namespace BitskiWPPluginBoilerplate\core;
+
+class Helpers
+{
+    /**
+     * Static getter for example functionality.
+     * Returns the string argument.
+     *
+     * @param string $exampleString
+     *
+     * @return string
+     */
+    public static function getExampleString(string $exampleString): string
+    {
+        // Add your functionality here.
+
+        // Returns the string argument.
+        return $exampleString;
+    }
+}
